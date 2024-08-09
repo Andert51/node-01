a166336af02114ce94626309eba47008e69ba062
Andert51 <andert05143@gmail.com>
2024-08-08 23:06:48 -0600

    Added src

---
+++ src/frutas.js
@@ -0,0 +1,11 @@
+var a = 10
+var a = "10"
+let b = 10
+
+const frutas = ['banana', 'cereza', 'manzana', 'pera', 'sandia']
+
+// frutas.forEach((fruta) => {
+//     console.log('@@@ fruta => ', fruta)
+// })
+
+module.exports = frutas
