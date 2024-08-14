9faf95df519d0e4e8594bf5b37eb24d39c5a4f23
Andres <andert05143@gmail.com>
2024-08-14 17:03:59 -0600

    Repo Finished

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-const frutas = require('./frutas')
+const frutas = require('./frutas') //exportacion que se guarda en una constante, es una exportacion a la vieja escuela 
 
-frutas.forEach((fruta, index) => {
+frutas.forEach((fruta, index) => { //Se guarda en una variable temporal y recorre
     console.log('@@@ fruta => ', index, fruta)
 })
+
+//Este archivo suele contener lo mas importante de la aplicacion, .gitigonre hace que se ignoren ciertos archivos del proyecto para no subirse a la nube, 
+//En este ejemplo se ignoran los modulos de node
+//Uso de NPM y Git en general, Javascript 
--- src/frutas.js
+++ src/frutas.js
@@ -1,8 +1,8 @@
-var a = 10
-var a = "10"
-let b = 10
+var a = 10 //No es necesario usar punto y coma
+var a = "10" //Las variables pueden ser cualquier cosa
+let b = 10 //Variable que no puede cambiar de valor despues
 
-const frutas = ['banana', 'cereza', 'manzana', 'pera', 'sandia']
+const frutas = ['banana', 'cereza', 'manzana', 'pera', 'sandia'] //Los arrays no necesitan comillas dobles, usan comillas simples
 
 // frutas.forEach((fruta) => {
 //     console.log('@@@ fruta => ', fruta)
